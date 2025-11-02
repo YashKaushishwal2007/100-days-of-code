ef576c5a242fcdb013661fd13fa26e0b5bd59bf4
Yashkaushiswal2007 <yash010707@gmail.com>
2025-11-02 22:19:05 +0530

    Initial commit

---
+++ 13 code.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+
+    int rows;
+    int coloums;
+    char symbol;
+
+    printf("enter the number of rows:");
+    scanf("%d",&rows);
+
+    printf("enter the number of coloum:");
+    scanf("%d",&coloums);
+
+    scanf ("%c");
+    printf("eneter the number symbol:");
+    scanf("%c",&symbol);
+
+    for( int i=1;i<=rows;i++)
+    {
+        for
+            (int j=1;j<=coloums;j++)
+            {
+        printf("%c",symbol);
+    }
+printf("\n");
+    }
+
+return 0;
+}
