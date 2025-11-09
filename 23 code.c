3debd94246d701aef069168580513e10373d03ab
Yashkaushiswal2007 <yash010707@gmail.com>
2025-11-09 13:46:41 +0530

    Initial commit

---
+++ 23 code.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int i; 
+
+    
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    printf("Numbers from 1 to %d:\n", n);
+
+
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i); 
+    }
+
+    
+    printf("\n");
+
+    return 0;
+}
