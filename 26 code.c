bbbd32824cc30e6e46ea88a90bcbd62b9c0ef35f
Yashkaushiswal2007 <yash010707@gmail.com>
2025-11-09 14:00:47 +0530

    Initial commit

---
+++ 26 code.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n; 
+    int i; 
+    unsigned long long factorial = 1;
+
+    
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    
+    if (n < 0) {
+        printf("Error: Factorial is not defined for negative numbers.\n");
+    } else {
+        
+        for (i = 1; i <= n; ++i) {
+            factorial *= i; 
+        }
+
+    
+        printf("Factorial of %d = %llu\n", n, factorial);
+    }
+
+    return 0;
+}
