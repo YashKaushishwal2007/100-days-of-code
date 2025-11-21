7657051451099af7507abc7b7423d3be0f390000
Yashkaushiswal2007 <yash010707@gmail.com>
2025-11-21 10:12:02 +0530

    Initial commit

---
+++ 45.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    int i, j;
+    int rows = 5;
+
+    printf("Pattern:\n");
+
+    
+    for (i = 1; i <= rows; i++) {
+    
+
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
