671c6421156c874753f1068122523db61cf627e8
Yashkaushiswal2007 <yash010707@gmail.com>
2025-11-21 10:13:16 +0530

    Initial commit

---
+++ 46.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main() {
+    int i, j;
+    int rows = 5;
+
+    printf("Pattern:\n");
+
+    
+    for (i = 1; i <= rows; i++) {
+        
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        
+        printf("\n");
+    }
+
+    return 0;
+}
