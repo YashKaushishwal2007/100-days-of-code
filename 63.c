e8793dcff28e9c622e77bcef424e38cc140e4bd4
Yashkaushiswal2007 <yash010707@gmail.com>
2025-11-22 11:04:21 +0530

    Initial commit

---
+++ 63.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+    int arr[100], n, i, key, pos;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    
+    printf("Enter %d elements (in sorted order):\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to insert: ");
+    scanf("%d", &key);
+
+    
+    pos = n - 1;
+    while(pos >= 0 && arr[pos] > key) {
+        arr[pos + 1] = arr[pos];  
+        pos--;
+    }
+
+    arr[pos + 1] = key;  
+    n++;                 
+
+    
+    printf("Array after insertion:\n");
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
