3d3f53bd43b9444cee71af5b44e4db8221f34ee6
Yashkaushiswal2007 <yash010707@gmail.com>
2025-11-24 19:23:56 +0530

    Initial commit

---
+++ 75.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], t[10][10];
+    int r, c, i, j;
+
+    
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            t[j][i] = a[i][j];
+        }
+    }
+
+    
+    printf("\nTranspose of the matrix:\n");
+    for (i = 0; i < c; i++) {
+        for (j = 0; j < r; j++) {
+            printf("%d ", t[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
