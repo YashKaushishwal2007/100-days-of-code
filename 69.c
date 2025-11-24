c941c8c0a3cf89f56b146a09d02859480f4f4eb8
Yashkaushiswal2007 <yash010707@gmail.com>
2025-11-24 19:06:54 +0530

    Initial commit

---
+++ 69.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    int arr[100], n, i;
+    int largest = INT_MIN, secondLargest = INT_MIN;
+
+    
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    
+    printf("Enter array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    for (i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    
+    if (secondLargest == INT_MIN) {
+        printf("No second largest element (all elements are equal).\n");
+    } else {
+        printf("The second largest element is: %d\n", secondLargest);
+    }
+
+    return 0;
+}
