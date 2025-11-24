cdf4212529b4d55fae97ef4440d7d3517efaefb2
Yashkaushiswal2007 <yash010707@gmail.com>
2025-11-24 19:04:05 +0530

    Initial commit

---
+++ 66.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, num, i, pos;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+
+    printf("Enter the sorted array elements:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    
+    printf("Enter element to insert: ");
+    scanf("%d", &num);
+
+    
+    pos = n;
+    for (i = 0; i < n; i++) {
+        if (num < arr[i]) {
+            pos = i;
+            break;
+        }
+    }
+
+    
+    for (i = n; i > pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos] = num;
+    n++;
+
+    
+    printf("Array after insertion:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+
