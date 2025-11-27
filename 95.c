6f5a6d28cfc1a3944baf3c2eb9aebd9a725481fa
Yashkaushiswal2007 <yash010707@gmail.com>
2025-11-27 19:04:10 +0530

    Initial commit

---
+++ 95.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char *str, int start, int end) {
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    char str[200];
+    int start = 0, i;
+
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+
+    int len = strlen(str);
+
+    for (i = 0; i <= len; i++) {
+        if (str[i] == ' ' || str[i] == '\0' || str[i] == '\n') {
+            reverse(str, start, i - 1);
+            start = i + 1;
+        }
+    }
+
+    printf("Modified sentence: %s", str);
+
+    return 0;
+}
+
