f064023034c58c1dd41b4f0944db5f394cb9b2cb
Yashkaushiswal2007 <yash010707@gmail.com>
2025-12-01 17:00:02 +0530

    Initial commit

---
+++ 142.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+    
+    printf("Enter student name: ");
+    fgets(s.name, sizeof(s.name), stdin);
+
+    printf("Enter roll number: ");
+    scanf("%d", &s.roll_no);
+
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    
+    printf("\nStudent Details:\n");
+    printf("Name: %s", s.name);
+    printf("Roll Number: %d\n", s.roll_no);
+    printf("Marks: %.2f\n", s.marks);
+
+    return 0;
+}
