2452e6b98e72317706279b4c1a04b5f7e027ea8c
Yashkaushiswal2007 <yash010707@gmail.com>
2025-12-01 16:54:21 +0530

    Initial commit

---
+++ 137.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+enum Menu { ADD = 1, SUBTRACT, MULTIPLY };
+
+int main() {
+    enum Menu choice;
+    int a, b;
+
+    
+    printf("Menu:\n1. ADD\n2. SUBTRACT\n3. MULTIPLY\n");
+    printf("Enter your choice (1-3): ");
+    scanf("%d", &choice);
+
+    
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    
+    switch (choice) {
+        case ADD:
+            printf("Result: %d\n", a + b);
+            break;
+        case SUBTRACT:
+            printf("Result: %d\n", a - b);
+            break;
+        case MULTIPLY:
+            printf("Result: %d\n", a * b);
+            break;
+        default:
+            printf("Invalid choice!\n");
+    }
+
+    return 0;
+}
