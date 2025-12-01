03c2a14803796b4ec02e27ad8331ad057e538c09
Yashkaushiswal2007 <yash010707@gmail.com>
2025-12-01 17:02:40 +0530

    Initial commit

---
+++ 145.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+// Function to print a student structure
+void printStudent(struct Student s) {
+    printf("\nStudent Details:\n");
+    printf("Name: %s", s.name);
+    printf("Roll Number: %d\n", s.roll_no);
+    printf("Marks: %.2f\n", s.marks);
+}
+
+int main() {
+    struct Student s;
+
+    
+    printf("Enter student name: ");
+    fgets(s.name, sizeof(s.name), stdin);
+
+    printf("Enter roll number: ");
+    scanf("%d", &s.roll_no);
+
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    
+    printStudent(s);
+
+    return 0;
+}
